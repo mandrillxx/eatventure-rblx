5023e525f9a849b6dff41fca282aef99fcb084e0
zkzkatz <mandrillx@protonmail.com>
2023-08-31 21:15:06 -0400

    add: more ui library work

--- src/client/stories/button.story.tsx
+++ src/client/stories/button.story.tsx
@@ -11,20 +11,53 @@ Roact.setGlobalConfig({
 });
 
 interface IControls {
+	labelText: string;
+	leadingIcon?: string;
+	showLeadingIcon?: boolean;
+	trailingIcon?: string;
+	showTrailingIcon?: boolean;
+	buttonType: "Default" | "Destructive" | "Secondary" | "Ghost" | "Success";
+	buttonTextSize: "sm" | "md" | "lg" | "xl" | "2xl";
+	buttonSize: "sm" | "md" | "lg" | "icon";
 	useBoldFont: boolean;
 }
 
 withHookDetection(Roact);
 const story: IRoactStoryWithControls<IControls> = {
 	controls: {
+		labelText: "Hello world",
+		buttonSize: "sm",
+		buttonTextSize: "sm",
+		buttonType: "Default",
 		useBoldFont: false,
 	},
 
 	name: "Button.story",
 	roact: Roact,
 	story: ({ controls }) => {
-		const { useBoldFont } = controls;
-		return <Button Text="Hello world" onActivated={() => print("Activated!")} Bold={useBoldFont} />;
+		const {
+			labelText,
+			leadingIcon,
+			showLeadingIcon,
+			trailingIcon,
+			showTrailingIcon,
+			useBoldFont,
+			buttonSize,
+			buttonTextSize,
+			buttonType,
+		} = controls;
+		return (
+			<Button
+				Type={buttonType}
+				Size={buttonSize}
+				TextSize={buttonTextSize}
+				Bold={useBoldFont}
+				Text={labelText}
+				LeadingIcon={showLeadingIcon ? leadingIcon : undefined}
+				TrailingIcon={showTrailingIcon ? trailingIcon : undefined}
+				onActivated={() => print("Activated!")}
+			/>
+		);
 	},
 	summary: "The button component.",
 };
